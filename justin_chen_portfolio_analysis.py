c4bba35a982e60130715f19750f507c37f8247c0
Justin <JustinChen3946@gmail.com>
2023-10-13 20:05:09 -0400

    Asset values + unrealized pnl

--- justin_chen_portfolio_analysis.py
+++ justin_chen_portfolio_analysis.py
@@ -47,12 +47,13 @@ class PortfolioAnalysis:
     hand and the portfolio's total value, from the end of June to the end of September
     """
 
-    def __init__(self, excel_path:str):
+    def __init__(self, excel_path:str, export_clean_data:bool=True):
         '''
         Creates a portfolio analysis that reads an .xlsx file path (ie. dummy_data) and cleans it by replacing missing data with either
         data found on Y-Finance, or linear regression if it still can't be found. Then, it calculates asset values and unrealized returns, then 
         stores them inside parameters/arguments
         @param excel_path The path to the excel file
+        @param export_clean_data Whether Whether or not to export the cleaned data. `True` by default.
         '''
 
         # Just a paranoid error checking thing because I might use this library down the line - make sure the excel file exists
@@ -60,18 +61,23 @@ class PortfolioAnalysis:
         assert os.path.isfile(excel_path), 'The path provided is not a file'
         assert os.path.splitext(excel_path)[-1].lower() == ".xlsx", 'The path provided is not an excel file'
 
-        # Mistake learned we're not rewriting Pandas this year
+        # Mistake learned, we're not rewriting Pandas this year
 
         '''
         A list of the column names for items that should be numeric
         '''
         self.NUMERIC_COLS = ["Quantity", "UnitCost", "MarketPrice"]
-
+        
         '''
         A dictionary that stores all the CSVs of inside the excel as dataframes
         '''
         self.excel_dfs:dict = pd.read_excel(excel_path, sheet_name=None)       # None loads all worksheets as a dict
+        self.excel_dfs:dict = dict(sorted(self.excel_dfs.items()))             # Sort the dictionary
+        #self.clean_data(export_clean_data)
         self.clean_data(False)
+
+        self.asset_value()
+        self.unrealized_returns()
 
     def clean_data(self, export:bool):
         '''
@@ -83,6 +89,9 @@ class PortfolioAnalysis:
         IGNORE_CHARS = r'["\+\'$ ]'
         NAN_RE = r'nan'
 
+        # ⭐ Going to create a dict that stores unit cost; we'll see what that does later
+        unit_cost:dict = dict()
+        
         for key in self.excel_dfs.keys(): # For each sheet
 
             # Step 1: Clean Data
@@ -93,6 +102,7 @@ class PortfolioAnalysis:
                 self.excel_dfs[key][to_convert_cols] = pd.to_numeric(self.excel_dfs[key][to_convert_cols], errors='coerce')
 
             # Step 2: Find Missing Data, if it exists
+
             # First, let's take care of market price
             for index, row in self.excel_dfs[key].iterrows():
 
@@ -112,16 +122,51 @@ class PortfolioAnalysis:
                         
                     except:
                         # If we get to this place, it means the stock data does not exist. If it doesn't exist, then 
-                        row["MarketPrice"] = 0
-            
-            print("---")
-            print(self.excel_dfs[key])
+                        self.excel_dfs[key].loc[index, "MarketPrice"] = 0
+
+                # ⭐ Next, let's take care of UnitPrice
+                # @see: https://www.investopedia.com/terms/u/unitcost.asp#:~:text=The%20unit%20cost%2C%20also%20known,this%20price%20is%20company%20profit.
+                # Unit price *is* our "break even point", which doesn't really change between months (since it's when we bought the stock)
+                
+                # Initialize UnitCost dictionary if it's empty
+                if (not (row["Stock"] in unit_cost)):
+                    unit_cost[row["Stock"]] = -1        # Unit costs can never be -1 so this is a placeholder
+
+                # Try to find a unit cost value
+                if not(isnan(row["UnitCost"])):
+                    unit_cost[row["Stock"]] = row["UnitCost"]
+
+        # ⭐ If a stock does not have a UnitCost, use the 1st MarketPrice we have
+        for stock_ticker in unit_cost:
+            for key in self.excel_dfs.keys():
+                if unit_cost[stock_ticker] == -1:   # if we found the marketprice, stop.
+                    filtered_row = self.excel_dfs[key].query(f"Stock == '{stock_ticker}'")
+                    unit_cost[stock_ticker] = filtered_row["MarketPrice"]
+
+        # ⭐ Loop back into the dataframe and fill in the missing UnitCosts
+        for key in self.excel_dfs.keys(): # For each sheet
+            for index, row in self.excel_dfs[key].iterrows():
+                if isnan(row["UnitCost"]):
+                    self.excel_dfs[key].loc[index, "UnitCost"] = unit_cost[row["Stock"]]
+
+            # If needed, print dfs here
+            # print(self.excel_dfs[key])
+            # print("-------------")
+
+        # If needed, export
+        if (export):
+            self.export_data()
 
     
     def export_data(self):
         '''
         Exports a cleaned data to cleaned_data.xlsx with all 3 sheets of excel files
         '''
+        # @see https://pandas.pydata.org/docs/reference/api/pandas.ExcelWriter.html
+
+        with pd.ExcelWriter("cleaned_data.xlsx") as writer:
+            for sheet_name, sheet in self.excel_dfs.items():
+                sheet.to_excel(writer, sheet_name=sheet_name, index=False)
 
     def asset_value(self):
         '''
@@ -129,11 +174,45 @@ class PortfolioAnalysis:
         If there is no position for a certain equity during a given month, its value is 0.
         '''
 
+        # Dataframe to store all the asset values
+        asset_df = pd.DataFrame()
+
+        for sheet_date, sheet in self.excel_dfs.items():
+            # Note to self: pandas brackets == columns
+            for index, row in sheet.iterrows():
+                asset_df.loc[row["Stock"], sheet_date] = row["MarketPrice"] * row["Quantity"]
+        
+        asset_df = asset_df.fillna(0)      # If no position, its value is 0
+
+        # Add a row for net asset values
+        # According to https://www.wallstreetprep.com/knowledge/net-asset-value-nav/, Net Asset Value (NAV) = Fund Assets – Fund Liabilities. 
+        # We don't have liabilities (I hope, because I'd be kinda mad if we're not allowed to short stuff because "liability" and still have debt) so it's just net assets
+        
+        asset_df.loc["Net Asset Value", :] = asset_df.sum()
+        self.asset_values = asset_df
+
     def unrealized_returns(self):
         '''
         Calculates the unrealized returns of each stock and exports it as a dataframe.
         If there is no unrealized gain/loss to be calculated for a given stock during a given month, its value is 0.
         '''
+
+        # Unrealized returns according to Investopedia is just the total value of an equity if we subtract the unit costs
+        # In other words, (marketPrice - unitCosts) * quantity
+        # We could use the net asset value dataframe to do half the calculations, but since we're already looping through the CSVs
+        # to calculate total value of our unit costs, it's much neater to just not call net asset value
+
+        # Dataframe to store all the unrealized returns
+        ureturns_df = pd.DataFrame()
+
+        for sheet_date, sheet in self.excel_dfs.items():
+            for index, row in sheet.iterrows():
+                ureturns_df.loc[row["Stock"], sheet_date] = (row["MarketPrice"] - row["UnitCost"]) * row["Quantity"]
+        
+        ureturns_df = ureturns_df.fillna(0)      # If no position, its value is 0
+
+        print(ureturns_df)
+        self.unrealized_pnl = ureturns_df
 
 
 if __name__ == "__main__":  # Do not change anything here - this is how we will test your class as well.
